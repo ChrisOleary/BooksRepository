f60954b93c5461c12cb3f5bb8ea0121091023ed3
Chris O'leary <chris_oleary@hotmail.co.uk>
2020-03-03 07:13:16 +0000

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using BooksRepository.DAL;
+using BooksRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BooksRepository.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            //use LINQ to return data from repository
+            var books = from book in _bookRepository.GetBooks()
+                        select book;
+            return View(books);
+        }
+        private IBookRepository _bookRepository;
+        public BookController()
+        {
+            this._bookRepository = new BookRepository(new BookContext());
+        }
+
+        public ViewResult Details(int id)
+        {
+            Book student = _bookRepository.GetBookByID(id);
+            return View(student);
+        }
+
+    }
+}
---
+++ DAL/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using BooksRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BooksRepository.DAL
+{
+    public class BookContext : DbContext
+    {
+        public BookContext()
+            : base("name=BooksRepositoryConnectionString")
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
